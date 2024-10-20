6ceba5e9d5ca21db981256068f3157db81739ee0
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-10-20 16:38:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express from "express"
 import cors from "cors"
-import rotaFuncionario from "./routes/Funcionario.js"
 
 
 const app = express();
 app.use(cors())
 app.use(express.json());
-
-app.use("/funcionario", rotaFuncionario)
 
 
 // Iniciar o servidor
